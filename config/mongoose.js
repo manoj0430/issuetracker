0d9765cc4177fb15a729c728898940dad86b6f10
manasa lingabathula <manojl0430@gmail.com>
2024-02-12 18:15:30 +0530

    Resolve mongoose error

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const db = mongoose.connection;
-
+mongoose.set('bufferCommands', false);
 main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/issuetracker');
-  mongoose.set('bufferCommands', false);
+  
   console.log("My db is working");
   
 }
